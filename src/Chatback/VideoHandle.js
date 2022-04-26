c4ea3256c2df7ba61886aaeb36b5de4ca4535a96
Naser-IR <92335369+Naser-IR@users.noreply.github.com>
2022-04-26 14:58:15 +0300

    Update VideoHandle.js

--- src/Chatback/VideoHandle.js
+++ src/Chatback/VideoHandle.js
@@ -1,7 +1,7 @@
 
 function Vhandle({video}){
     return (<div>
-        <video width="320" height="240" controls>
+        <video width="320" id="vid" height="240" controls>
         <source src={video} type="video/mp4"></source></video>
        </div>)
 }
